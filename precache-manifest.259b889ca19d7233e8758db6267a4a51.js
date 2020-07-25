0cdbf6497c124124c1c58e96c922efa7e2b142a7
Rakesh <justdecodeme@hotmail.com>
2020-07-25 23:13:45 +0530

    Updates

---
+++ precache-manifest.259b889ca19d7233e8758db6267a4a51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d8aa42afddbde3558568973f12d3e0",
+    "url": "/react-bosch-system-test/index.html"
+  },
+  {
+    "revision": "6129fa40fae5804fafa3",
+    "url": "/react-bosch-system-test/static/css/main.e8c4e00c.chunk.css"
+  },
+  {
+    "revision": "081f67c0daad0c21fe72",
+    "url": "/react-bosch-system-test/static/js/2.6d8371fa.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/react-bosch-system-test/static/js/2.6d8371fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6129fa40fae5804fafa3",
+    "url": "/react-bosch-system-test/static/js/main.7122e913.chunk.js"
+  },
+  {
+    "revision": "6e6370892ee0186e9dea",
+    "url": "/react-bosch-system-test/static/js/runtime-main.977c063f.js"
+  }
+]);
